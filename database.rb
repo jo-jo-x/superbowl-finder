c192dd8be86702cff4d1d38bfa3e8a59e1a43017
Maurice Ford <mauriceford@gmail.com>
2025-02-22 02:15:25 +0000

    rnum code complete

--- database.rb
+++ database.rb
@@ -34,60 +34,59 @@ teams.push(
 "Tennessee Titans", 
 "Washington Redskins"
 )
+arraysam = [3, 8, 17, 29, 50, 51, 60]
 
-test = 40
-
+test = arraysam.sample
+# =begin
 if test.to_s.length == 2
-  if test.to_s.chars.map(&:to_i)[0] == 0
-    rnum = "No"
-  elsif test.to_s.chars.map(&:to_i)[0] =< 3
+  if test.to_s.chars.map(&:to_i)[0] <= 3
     rnuma = "X"
-    rnuma = rnuma * (test.to_s.chars.map(&:to_i)[0] - 1)
+    rnuma = rnuma * test.to_s.chars.map(&:to_i)[0]
   elsif test.to_s.chars.map(&:to_i)[0] == 4
     rnuma = "XL"
   elsif test.to_s.chars.map(&:to_i)[0] == 5
-    rnuma = "L"
-  elsif test.to_s.chars.map(&:to_i)[0] =< 8
+    if test.to_s.chars.map(&:to_i)[1] == 0
+      rnuma = "50"
+    else
+      rnuma = "L"
+    end
+  elsif test.to_s.chars.map(&:to_i)[0] <= 8
     rnumz = "X"
-    rnumx = rnumz * (test.to_s.chars.map(&:to_i)[0] - 3)
+    rnumx = rnumz * (test.to_s.chars.map(&:to_i)[0] - 5)
     rnuma = "L" + rnumx
   else
     rnuma = "XC"
   end
   if test.to_s.chars.map(&:to_i)[1] == 0
     rnumb = ""
-  elsif test.to_s.chars.map(&:to_i)[1] =< 3
+  elsif test.to_s.chars.map(&:to_i)[1] <= 3
     rnumb = "I"
-    rnumb = rnumb * (test.to_s.chars.map(&:to_i)[1] - 1)
+    rnumb = rnumb * test.to_s.chars.map(&:to_i)[1]
   elsif test.to_s.chars.map(&:to_i)[1] == 4
     rnumb = "IV"
   elsif test.to_s.chars.map(&:to_i)[1] == 5
     rnumb = "V"
-  elsif test.to_s.chars.map(&:to_i)[1] =< 8
+  elsif test.to_s.chars.map(&:to_i)[1] <= 8
     rnumz = "I"
-    rnumi = rnumz * (test.to_s.chars.map(&:to_i)[1] - 3)
+    rnumi = rnumz * (test.to_s.chars.map(&:to_i)[1] - 5)
     rnumb = "V" + rnumi
   else
     rnumb = "IX"
   end
-  if rnum = "No"
-    rnum = "Invalid Number"
-  else
-    rnum = rnuma + rnumb
-  end
+  rnum = rnuma + rnumb
 else
   if test.to_s.chars.map(&:to_i)[0] == 0
     rnumb = ""
-  elsif test.to_s.chars.map(&:to_i)[0] =< 3
+  elsif test.to_s.chars.map(&:to_i)[0] <= 3
     rnumb = "I"
-    rnumb = rnumb * (test.to_s.chars.map(&:to_i)[0] - 1)
+    rnumb = rnumb * test.to_s.chars.map(&:to_i)[0]
   elsif test.to_s.chars.map(&:to_i)[0] == 4
     rnumb = "IV"
   elsif test.to_s.chars.map(&:to_i)[0] == 5
     rnumb = "V"
-  elsif test.to_s.chars.map(&:to_i)[0] =< 8
+  elsif test.to_s.chars.map(&:to_i)[0] <= 8
     rnumz = "I"
-    rnumi = rnumz * (test.to_s.chars.map(&:to_i)[0] - 3)
+    rnumi = rnumz * (test.to_s.chars.map(&:to_i)[0] - 5)
     rnumb = "V" + rnumi
   else
     rnumb = "IX"
@@ -95,10 +94,10 @@ else
   rnum = rnumb
 end
 
-
-
-
 print rnum
+#=end
+
+
 
 sb_list = Hash.new
 
