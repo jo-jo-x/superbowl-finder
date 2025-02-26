037c1c31540bf57a12cb6b116f69cb2ffc47d45c
Maurice Ford <mauriceford@gmail.com>
2025-02-26 01:48:45 +0000

    final code, updated arrays to hashes

--- bowlfinder.rb
+++ bowlfinder.rb
@@ -6,7 +6,7 @@ class Bowling
   end
 
   def y_or_n
-    # List of teams
+    # Hash of teams
     teams = { 
       :t0 => "Arizona Cardinals", 
       :t1 => "Atlanta Falcons", 
@@ -40,40 +40,40 @@ class Bowling
       :t29 => "Tennessee Titans", 
       :t30 => "Washington Redskins" 
       }
-      # First array of competing teams
+      # First hash of competing teams
       team1 = { 
       # 1960s  
       "ta1966" => :t12, "ta1967" => :t10, "ta1968" => :t20, "ta1969" => :t16, 
       # 1970s 
-      :ta1970 => :t2, :ta1971 => :t8, :ta1972 => :t15, :ta1973 => :t16, :ta1974 => :t23, :ta1975 => :t9, :ta1976 => :t21, :ta1977 => :t8, :ta1978 => :t23, :ta1979 => :t14, 
+      "ta1970" => :t2, "ta1971" => :t8, "ta1972" => :t15, "ta1973" => :t16, "ta1974" => :t23, "ta1975" => :t9, "ta1976" => :t21, "ta1977" => :t8, "ta1978" => :t23, "ta1979" => :t14, 
       # 1980s
-      :ta1980 => :t21, :ta1981 => :t25, :ta1982 => :t15, :ta1983 => :t30, :ta1984 => :t15, :ta1985 => :t6, :ta1986 => :t10, :ta1987 => :t30, :ta1988 => :t7, :ta1989 => :t25, 
+      "ta1980" => :t21, "ta1981" => :t25, "ta1982" => :t15, "ta1983" => :t30, "ta1984" => :t15, "ta1985" => :t6, "ta1986" => :t10, "ta1987" => :t30, "ta1988" => :t7, "ta1989" => :t25, 
       # 1990s
-      4, 30, 4, 8, 24, 8, 17, 10, 9, 27, 
+      "ta1990" => :t4, "ta1991" => :t30, "ta1992" => :t4, "ta1993" => :t8, "ta1994" => :t24, "ta1995" => :t8, "ta1996" => :t17, "ta1997" => :t10, "ta1998" => :t9, "ta1999" => :t27, 
       # 2000s
-      3, 27, 21, 5, 17, 26, 11, 18, 23, 20, 
+      "ta2000" => :t3, "ta2001" => :t27, "ta2002" => :t21, "ta2003" => :t5, "ta2004" => :t17, "ta2005" => :t26, "ta2006" => :t11, "ta2007" => :t18, "ta2008" => :t23, "ta2009" => :t20, 
       # 2010s
-      23, 18, 3, 26, 17, 5, 17, 22, 17, 25, 
+      "ta2010" => :t23, "ta2011" => :t18, "ta2012" => :t3, "ta2013" => :t26, "ta2014" => :t17, "ta2015" => :t5, "ta2016" => :t17, "ta2017" => :t22, "ta2018" => :t17, "ta2019" => :t25, 
       # 2020s 
-      12, 14, 12, 25, 12
+      "ta2020" => :t12, "ta2021" => :t14, "ta2022" => :t12, "ta2023" => :t25, "ta2024" => :t12 
       }
-      # Second array of competing teams
-      team2 = [
+      # Second hash of competing teams
+      team2 = { 
       # 1960s   
-      10, 21, 2, 12, 
+      "tb1966" => :t10, "tb1967" => :t21, "tb1968" => :t2, "tb1969" => :t12, 
       # 1970s 
-      8, 15, 30, 15, 16, 23, 16, 9, 8, 23, 
+      "tb1970" => :t8, "tb1971" => :t15, "tb1972" => :t30, "tb1973" => :t15, "tb1974" => :t16, "tb1975" => :t23, "tb1976" => :t16, "tb1977" => :t9, "tb1978" => :t8, "tb1979" => :t23, 
       # 1980s
-      22, 7, 30, 13, 25, 17, 18, 9, 25, 9, 
+      "tb1980" => :t22, "tb1981" => :t7, "tb1982" => :t30, "tb1983" => :t13, "tb1984" => :t25, "tb1985" => :t17, "tb1986" => :t18, "tb1987" => :t9, "tb1988" => :t25, "tb1989" => :t9, 
       # 1990s
-      18, 4, 8, 4, 25, 23, 10, 9, 1, 29, 
+      "tb1990" => :t18, "tb1991" => :t4, "tb1992" => :t8, "tb1993" => :t4, "tb1994" => :t25, "tb1995" => :t23, "tb1996" => :t10, "tb1997" => :t9, "tb1998" => :t1, "tb1999" => :t29, 
       # 2000s
-      18, 17, 28, 17, 22, 23, 6, 17, 0, 11, 
+      "tb2000" => :t18, "tb2001" => :t17, "tb2002" => :t28, "tb2003" => :t17, "tb2004" => :t22, "tb2005" => :t23, "tb2006" => :t6, "tb2007" => :t17, "tb2008" => :t0, "tb2009" => :t11, 
       # 2010s
-      10, 17, 25, 9, 26, 9, 1, 17, 14, 12, 
+      "tb2010" => :t10, "tb2011" => :t17, "tb2012" => :t25, "tb2013" => :t9, "tb2014" => :t26, "tb2015" => :t9, "tb2016" => :t1, "tb2017" => :t17, "tb2018" => :t14, "tb2019" => :t12, 
       # 2020s 
-      28, 7, 22, 12, 22
-      ]
+      "tb2020" => :t28, "tb2021" => :t7, "tb2022" => :t22, "tb2023" => :t12, "tb2024" => :t22 
+      }
 
     puts "\n"
     puts "This is the NFL Super Bowl search tool! Search for a Super Bowl by YEAR or by NUMBER?"
@@ -152,7 +152,7 @@ class Bowling
             end
             rnum = rnumb
           end
-          puts "Super Bowl #{rnum} was the championship game for the #{self.input} season. The teams were the #{teams[team1[idx]]} versus the #{teams[team2[idx]]}."
+          puts "Super Bowl #{rnum} was the championship game for the #{self.input} season. The teams were the #{teams.fetch(team1.fetch("ta" + self.input.to_s))} versus the #{teams.fetch(team2.fetch("tb" + self.input.to_s))}."
           puts "For more information check this link: https://en.wikipedia.org/wiki/Super_Bowl_#{rnum}"
           puts "\n"
         end
@@ -224,7 +224,7 @@ class Bowling
             end
             rnum = rnumb
           end
-          puts "Super Bowl #{rnum} was the championship game for the #{date} season. The teams were the #{teams[team1[idx]]} versus the #{teams[team2[idx]]}."
+          puts "Super Bowl #{rnum} was the championship game for the #{date} season. The teams were the #{teams.fetch(team1.fetch("ta" + date.to_s))} versus the #{teams.fetch(team2.fetch("tb" + date.to_s))}."
           puts "For more information check this link: https://en.wikipedia.org/wiki/Super_Bowl_#{rnum}"
           puts "\n"
         end
